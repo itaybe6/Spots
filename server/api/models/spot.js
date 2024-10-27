2d1ab945088a5adfa9bf2193d612738fa748eb8d
איתי <@DESKTOP-5CAMIR6>
2024-10-27 12:56:09 +0200

    add new tables

---
+++ server/api/models/spot.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Review = require('./review'); // יבוא של הסכמה של ביקורות
+
+const SpotSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  placeId: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  location: {
+    lat: {
+      type: Number,
+      required: true
+    },
+    lng: {
+      type: Number,
+      required: true
+    }
+  },
+  type: {
+    type: String,
+    enum: ['Restaurant', 'Bar', 'Club', 'Spa', 'Playground', 'Workshop', 'Park'],
+    required: true
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  reviews: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Review'
+  }]
+});
+
+module.exports = mongoose.model('Spot', SpotSchema);
