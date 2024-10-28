554fd49a67f5203b37962cd4ef7a8b4c06775e0c
איתי <@DESKTOP-5CAMIR6>
2024-10-28 08:33:47 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import Map from "./components/Map";
-import { mapOptions } from "./components/MapConfiguration";
-import { useJsApiLoader } from "@react-google-maps/api";
-
 
 function App(){
-  const { isLoaded } = useJsApiLoader({
-    id: mapOptions.googleMapApikey,
-    googleMapsApiKey: mapOptions.googleMapApikey,
-  });
+
   return (
     <div className="App">
       <h1>
         React Google map Api
       </h1>
-      <Map isLoaded={isLoaded}/>
+      <Map />
     </div>
   )
 }
