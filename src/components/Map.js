9f17f38d6f9b7b52eff8769f8a0c2555a2e9e607
איתי <@DESKTOP-5CAMIR6>
2024-11-10 11:58:02 +0200

    fix the close bag

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,10 +3,14 @@ import { GoogleMap, InfoWindow, Marker, useJsApiLoader } from '@react-google-map
 import { mapOptions } from './MapConfiguration';
 import axios from 'axios';
 import PlaceInfo from './PlaceInfo';
-import CustomMarker from '../assets/images/images.png';
+
 import Drinks from '../assets/images/Drinks.png';
-import SpaIcon from '../assets/images/spa.webp';
-import Party from '../assets/images/party.avif';
+import Bar from '../assets/images/Bar.png';
+import Coffee from '../assets/images/Coffee.png';
+import Park from '../assets/images/Park.png';
+import Restaurant from '../assets/images/Restaurant.svg';
+import Spa from '../assets/images/Spa.png';
+
 
 
 const Map = () => {
@@ -34,6 +38,14 @@ const Map = () => {
     height: '90vh',
   };
 
+
+  const onCloseFunc = () => {
+    setSelectedPlace("")
+    const unwantedArrow = document.querySelectorAll('.gm-style-iw-t');
+    unwantedArrow.forEach(arrow => arrow.parentNode.removeChild(arrow));
+
+  }
+
   const fetchNearbyPlaces = async () => {
     try {
       const response = await axios.get('http://localhost:8010/proxy/maps/api/place/nearbysearch/json', {
@@ -44,7 +56,7 @@ const Map = () => {
           key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
         },
       });
-  
+
       const filteredPlaces = response.data.results
         .filter((place) =>
           place.types && // Check if place.types exists
@@ -59,20 +71,19 @@ const Map = () => {
             ? `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${place.photos[0].photo_reference}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
             : null
         }));
-  
+
       setPlaces(filteredPlaces);
-      console.log("Filtered Places:", filteredPlaces); 
+      console.log("Filtered Places:", filteredPlaces);
       // savePlaces(filteredPlaces);
 
-  
+
       localStorage.setItem("lastFetchTime", Date.now());
     } catch (error) {
       console.error('Error fetching places:', error);
     }
   };
-  
+
   const savePlaces = async (placesToSave) => {
-    console.log("dolev hereeeeee");
     try {
       console.log('Places to be saved:', placesToSave);
       const response = await axios.post('http://localhost:5001/api/save-places', { places: placesToSave });
@@ -84,37 +95,31 @@ const Map = () => {
 
   const getIconUrl = (placeType) => {
     const icons = {
-      bar: Drinks,
-      spa: SpaIcon, // Use your spa icon here
-      restaurant: CustomMarker,
-      cafe: CustomMarker,
-      night_club: Party,
-      park: CustomMarker,
-      // Add more types and their respective icons as needed
+        bar: Bar,
+        spa: Spa,
+        restaurant: Restaurant,
+        cafe: Coffee,
+        night_club: Drinks,
+        park: Park,
     };
 
-    return icons[placeType] || CustomMarker; // Default to CustomMarker if type not found
-  };
+
+    return icons[placeType] || Restaurant;
+};
 
 
   const handleReviewSubmit = async (id, review) => {
     try {
-      // Make API call to save the review
-      console.log(id,review)
       const response = await axios.post('http://localhost:5001/api/add-review', { id, review });
       console.log('Review submitted successfully:', response.data);
-      
-      // Update the selectedPlace with the new review in real-time
       if (selectedPlace) {
-        // Create a new review object with the response data
         const newReview = {
           ...review,
-          _id: response.data._id, // Assuming the response contains the new review ID
-          timestamp: Date.now(), // Add a timestamp for the new review
+          _id: response.data._id,
+          timestamp: Date.now(),
         };
-        // Update the reviews state of selectedPlace
         const updatedReviews = [...(selectedPlace.reviews || []), newReview];
-        setSelectedPlace(prev => ({ ...prev, reviews: updatedReviews })); // Update the state with the new reviews
+        setSelectedPlace(prev => ({ ...prev, reviews: updatedReviews }));
       }
     } catch (error) {
       console.error('Error submitting review:', error);
@@ -138,9 +143,9 @@ const Map = () => {
       const lastFetchTime = localStorage.getItem("lastFetchTime");
       const now = Date.now();
       const dayInMs = 24 * 60 * 60 * 1000;
-        // Check if 24 hours have passed
+      // Check if 24 hours have passed
       if (!lastFetchTime || now - lastFetchTime > dayInMs) {
-      fetchNearbyPlaces();
+        fetchNearbyPlaces();
       }
 
       fetchSavedPlaces();
@@ -149,70 +154,75 @@ const Map = () => {
   }, [isLoaded]);
 
   if (!isLoaded) return <div>Loading Google Maps...</div>
-  // console.log("neeed "+ JSON.stringify(places));
+
 
   return (
-      <div>
-        {/* Dropdown for selecting place type */}
-        <div style={{ margin: '10px 0' }}>
-          <label htmlFor="placeType" style={{ marginRight: '10px' }}>Select Place Type:</label>
-          <select
-            id="placeType"
-            value={selectedType}
-            onChange={(e) => setSelectedType(e.target.value)} // Update selected type on change
-            style={{ padding: '5px', fontSize: '16px' }}
-          >
-            <option value="bar">Bar</option>
-            <option value="restaurant">Restaurant</option>
-            <option value="cafe">Café</option>
-            <option value="night_club">Night Club</option>
-            <option value="spa">Spa</option>
-            <option value="park">Park</option>
-          </select>
-        </div>
-  
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={currentLocation}
-          zoom={14}
-          options={{
-            styles: mapOptions.mapTheme,
-          }}
+    <div>
+      <div style={{ margin: '10px 0' }}>
+        <label htmlFor="placeType" style={{ marginRight: '10px' }}>Select Place Type:</label>
+        <select
+          id="placeType"
+          value={selectedType}
+          onChange={(e) => setSelectedType(e.target.value)} // Update selected type on change
+          style={{ padding: '5px', fontSize: '16px' }}
         >
-          {placesget.map((place) => (
-            (place.geometry?.location || place.location) ? (
-              <Marker
-                key={place.place_id || place._id}
-                position={{
-                  lat: place.geometry?.location?.lat || place.location?.lat,
-                  lng: place.geometry?.location?.lng || place.location?.lng,
-                }}
-                options={{
-                  icon: {
-                    url: getIconUrl(place.type) ,
-                    scaledSize: new window.google.maps.Size(20, 20),
-                  },
-                }}
-                onClick={() => setSelectedPlace(place)}
-              />
-            ) : null 
-          ))}
-  
-          {selectedPlace && (
-            <InfoWindow
-              position={{
-                lat: selectedPlace.geometry?.location?.lat || selectedPlace.location?.lat,
-                lng: selectedPlace.geometry?.location?.lng || selectedPlace.location?.lng,
-              }}
-              onCloseClick={() => setSelectedPlace(null)}
-            >
-              <PlaceInfo selectedPlace={selectedPlace} onReviewSubmit={handleReviewSubmit} />
-
-            </InfoWindow>
-          )}
-        </GoogleMap>
+          <option value="bar">Bar</option>
+          <option value="restaurant">Restaurant</option>
+          <option value="cafe">Café</option>
+          <option value="night_club">Night Club</option>
+          <option value="spa">Spa</option>
+          <option value="park">Park</option>
+        </select>
       </div>
-    );
-  };
-  
-  export default Map;
+
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={currentLocation}
+        zoom={14}
+        options={{
+          styles: mapOptions.mapTheme,
+        }}
+      >
+        {placesget.map((place) => (
+          (place.geometry?.location || place.location) ? (
+            <Marker
+              key={place.place_id || place._id}
+              position={{
+                lat: place.geometry?.location?.lat || place.location?.lat,
+                lng: place.geometry?.location?.lng || place.location?.lng,
+              }}
+              options={{
+                icon: {
+                  url: getIconUrl(place.type),
+                  scaledSize: new window.google.maps.Size(20, 20),
+           
+                },
+              }}
+              onClick={() => setSelectedPlace(place)}
+            />
+          ) : null
+        ))}
+
+        {selectedPlace && (
+          <InfoWindow
+            position={{
+              lat: selectedPlace.geometry?.location?.lat || selectedPlace.location?.lat,
+              lng: selectedPlace.geometry?.location?.lng || selectedPlace.location?.lng,
+            }}
+
+            onCloseClick={onCloseFunc}
+            options={{ zIndex: 999 }}
+          >
+
+
+            <PlaceInfo selectedPlace={selectedPlace} onReviewSubmit={handleReviewSubmit} />
+
+
+          </InfoWindow>
+        )}
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default Map;
